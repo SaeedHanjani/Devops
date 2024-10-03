46e49659437676af1020fad794a6e2cdd45d0f03
SaeedHanjani <101514925+SaeedHanjani@users.noreply.github.com>
2024-10-03 06:54:45 +0330

    Create init.sql

---
+++ init.sql
@@ -0,0 +1,10 @@
+CREATE TABLE users (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(100),
+    email VARCHAR(100)
+);
+
+INSERT INTO users (name, email) VALUES
+    ('Alice', 'alice@example.com'),
+    ('Bob', 'bob@example.com'),
+    ('Charlie', 'charlie@example.com');
